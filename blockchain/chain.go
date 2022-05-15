bf06a563e7de318e0731bf23aef4962b2b9c8cf3
Luan Phan <luanphandinh@outlook.com>
2022-05-15 16:31:19 +0800

    Add TODO

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -6,12 +6,16 @@ type BlockChain struct {
 }
 
 func (chain *BlockChain) AddBlock(data string) {
+	// @TODO: return error for this function
+	// This will fail in terms of empty blocks
+	// Caller need to create genesis block first.
 	prevBlock := chain.blocks[len(chain.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, newBlock)
 }
 
 func Genesis() *Block {
+	// @TODO: put this in either from env, or some config.
 	return NewBlock("Geneisis", []byte{})
 }
 
