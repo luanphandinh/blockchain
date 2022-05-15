5920f3439530de710b31a3e3c4970f8c0fbc5711
Luan Phan <luanphandinh@outlook.com>
2022-05-15 21:18:32 +0800

    add serialize & deserialize block

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -15,4 +21,31 @@ func NewBlock(data string, prevHash []byte) *Block {
 	block.Nonce = nonce
 
 	return block
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		// @TODO: return err, bad practice
+		log.Panic(err)
+	}
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+	if err != nil {
+		// @TODO: return err, bad practice
+		log.Panic(err)
+	}
+
+	return &block
 }
